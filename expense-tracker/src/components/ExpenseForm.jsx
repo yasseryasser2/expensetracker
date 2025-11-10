63e9292277a4efc34de0f209e2e0f5e42ed11328
Yasser Algburi <yasseralgburi@gmail.com>
2025-11-10 14:16:45 -0500

    project setup

---
+++ expense-tracker/src/components/ExpenseForm.jsx
@@ -0,0 +1,8 @@
+export default function ExpenseForm() {
+  const [description, setdescription] = useState("");
+  const [amount, setamount] = useState("");
+  const [category, setCategory] = useState("Food");
+  const [date, setDate] = useState("");
+
+  function handleSubmit(e) {}
+}
